1e12ca1437b338a6705e6976fc5173b86c697b28
Martin Picasarri <martinpicasarri@gmail.com>
2025-02-22 20:25:24 -0300

    Version 7-Filtro

--- src/componentes/ItemListContainer/ItemListContainer.jsx
+++ src/componentes/ItemListContainer/ItemListContainer.jsx
@@ -1,12 +1,60 @@
 import Item from "../Item/Item";
+import { fetchData } from '../../fetchData';
+import { useEffect, useState } from "react";
+import Loader from "../Loader/Loader";
 import "./ItemListContainer.css";
 
 function ItemListContainer({greetings}) {
+
+    const [todosLosProductos, setTodosLosProductos] = useState([]);
+    const [misProductos, setMisProductos] = useState([]); 
+    const [loading, setLoading] = useState(true);
+
+    const usarFiltro = (filtro) => {
+        switch (filtro) {
+            case "Todos":
+                setMisProductos(todosLosProductos);
+                break;
+            case "0-15000":
+                setMisProductos(todosLosProductos.filter(el => el.precio < 15000))
+                break;
+            case "15000-30000":
+                setMisProductos(todosLosProductos.filter(el => el.precio >= 15000))
+                break;
+            default:
+                break;
+        };
+    };
+
+    useEffect(() => {
+        fetchData().then(response => {
+            setTodosLosProductos(response);
+            setMisProductos(response);
+            setLoading(false);
+        })
+            .catch(err => console.error(err));
+    }, []);
+
+
     return (
         <>
             <h1>{greetings}</h1>
+           
+            <div className="boton-filtro"> 
+                <button onClick={() => usarFiltro("Todos")}>TODOS</button>
+                <button onClick={() => usarFiltro("0-15000")}>0 - 15000</button>
+                <button onClick={() => usarFiltro("15000-30000")}>15000 - +</button>
+            </div>
+
             <div className="contenedor-cards">
-                      
+            {
+                    loading ? <Loader /> :
+                        misProductos.map((el, index) => {
+                            return (
+                                <Item key={index} nombre={el.nombre} precio={el.precio} />
+                            );
+                        })
+                }
             </div>
         </>
     );
--- src/productos.js
+++ src/productos.js
@@ -3,7 +3,7 @@ export const productos = [
         id: 1,
         nombre: "Pizza Clasica",
         descripcion: "Pizza con mozzarella y aceitunas",
-        precio: 1000,
+        precio: 10000,
         img: "",
     },
 
@@ -11,7 +11,7 @@ export const productos = [
         id: 2,
         nombre: "Pizza Napolitana",
         descripcion: "Pizza con mozzarella, aceitunas y tomate",
-        precio: 1200,
+        precio: 11000,
         img: "",
     },
 
@@ -19,7 +19,7 @@ export const productos = [
         id: 3,
         nombre: "Pizza de Jamon y Morron",
         descripcion: "Pizza con mozzarella, jamon y morron",
-        precio: 1500,
+        precio: 14000,
         img: "",
     },
 
@@ -27,7 +27,7 @@ export const productos = [
         id: 4,
         nombre: "Pizza de Anchoas",
         descripcion: "Pizza con salsa de tomate, anchoas y aceitunas negras",
-        precio: 1800,
+        precio: 18000,
         img: "",
     },
 
@@ -35,7 +35,31 @@ export const productos = [
         id: 5,
         nombre: "Pizza Fugazza",
         descripcion: "Pizza con cebolla, mozzarella y aceitunas negras",
-        precio: 2000,
+        precio: 14000,
         img: "",
+    },
+
+    {
+        id: 6,
+        nombre: "Pizza 4 Quesos",
+        descripcion: "Pizza con mozzarella, queso azul, queso parmesano y queso roquefort",
+        precio: 14500,
+        img: "",
+    },
+
+    {
+        id: 7,
+        nombre: "Pizza Margarita",
+        descripcion: "Pizza con mozzarella, tomate y albahaca",
+        precio: 15500,
+        img: "",
+    },
+
+    {
+        id: 8,
+        nombre: "Pizza Jamon Crudo y Rucula",
+        descripcion: "Pizza con mozzarella, jamon crudo y rucula",
+        precio: 22000,
+        img: "",    
     },
 ];
